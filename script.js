74ecbbe7569677f8eef7798653bcfedf442ec8a7
jigar <jigar.k@simformsolutions.com>
2023-01-24 17:14:21 +0530

    file added

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('Well-come To my jsvascript learning.')
+//addition number with closer
+function addnumber(firstnumber, secondnumber) {
+  var resultValue = "Result is : ";
+  function add() {
+    return resultValue + (firstnumber + secondnumber);
+  }
+  return add;
+}
+var myfunction = addnumber(20, 80);
+var result = myfunction();
+document.getElementById("demo").innerHTML = result;
+
+//Click Counter
+var incrementclickcount = (function()
+{
+    var counter = 0;
+    return function()
+    {
+        return ++counter;
+    }
+})();
