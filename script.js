e63e8e1d0fd71b48962886bedca424ee23250e63
jigar <jigar.k@simformsolutions.com>
2023-01-25 16:05:34 +0530

    file added

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('Well-come To my jsvascript learning.')
+const colorsarr = ["red", "blue", "white"];
+localStorage.setItem('colors', JSON.stringify(colorsarr));
+
+const numberarr = [1,2,3]
+localStorage.setItem('numbers',JSON.stringify(numberarr));
+
+
+const colordata = JSON.parse(localStorage.getItem('colors'));
+console.log(colordata);
+
+const numbersdata = JSON.parse(localStorage.getItem('numbers'));
+console.log(numbersdata);
+// clear local storage
+//localStorage.clear();
