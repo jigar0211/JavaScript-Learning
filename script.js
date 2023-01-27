016d88eee99420275eb345db817657e7c65553f3
jigar <jigar.k@simformsolutions.com>
2023-01-27 09:55:11 +0530

    file added

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('Well-come To my jsvascript learning.')
+function mydisplay(some)
+{
+    document.getElementById("demo1").innerHTML = some;
+}
+async function myfunction(){ return "Hello" }
+myfunction().then(
+    function(value) {mydisplay(value);},
+    function(error) {mydisplay(error);}
+);
