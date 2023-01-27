91d6ca88a8479b73596f38431c8ad1cde683b0c1
jigar <jigar.k@simformsolutions.com>
2023-01-27 15:07:29 +0530

    add for in and forof loop

--- script.js
+++ script.js
@@ -55,3 +55,18 @@ do{
         console.log(num);
     }
 }while(num<=50)
+//For-In Loop 
+const numbers = [45, 4, 9, 16, 25];
+let txt = "";
+numbers.forEach(myFunction);
+document.getElementById("demo").innerHTML = txt;
+function myFunction(value) {
+  txt += value + "<br>"; 
+}
+//For-Of Loop
+let language = "javascript"
+let text = ""
+for (let i of language){
+    text += i + "<br>"
+}
+document.getElementById("demo1").innerHTML = text;
