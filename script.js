e6576314dce47300e853c7f0070e2e5f5b73fdc6
jigar <jigar.k@simformsolutions.com>
2023-02-01 17:36:00 +0530

    file added

--- script.js
+++ script.js
@@ -1,38 +1,17 @@
-let x;
-let y = 182e5;
-let z = 182e-5;
-x = "Jigar"
-let fullname = "jigar kalariya02" 
-document.getElementById("demo1").innerHTML = x
-document.getElementById("demo2").innerHTML = fullname;
-//For concate
-document.getElementById("demo3").innerHTML = x +" "+ fullname;
-document.getElementById("demo4").innerHTML = y +"</br>"+ z;
-//else if condition
-var book = "maths";
-if(book == "history"){
-    document.getElementById("demo5").innerHTML = book;
-}
-else if ( book == "chemistry" ){
-    document.getElementById("demo5").innerHTML = book;
-}
-else if ( book == "maths" ){
-    document.getElementById("demo5").innerHTML = book;
-}
-else{
-    document.getElementById("demo5").innerHTML = "Unknown Book";
-}
-//Switch Case
-let a = 2;
-switch (a) {
-    case 1:
-        a = 'one';
-        break;
-    case 2:
-        a = 'two';
-        break;
-    default:
-        a = 'not found';
-        break;
-}
-document.getElementById("demo6").innerHTML = a;
+const UIcontroller = (function(){
+  //Privete vars and funcs
+  let text = "change the h1."
+  const textChange = function(){
+    const element = document.querySelector('h1');
+    element.textContent = text;
+  }
+
+  //Return public funcs and vars
+  return {
+    callTectChange: function(){
+      textChange();
+      console.log(text);
+    }
+  }
+})();
+UIcontroller.callTectChange();
