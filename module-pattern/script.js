ff562a4969d22ed5a34b0a494a3c0aaf0a5c782e
jigar0211 <122254160+jigar0211@users.noreply.github.com>
2023-02-02 16:56:21 +0530

    script content added

--- module-pattern/script.js
+++ module-pattern/script.js
@@ -1,1 +1,17 @@
+const UIcontroller = (function(){
+  //Privete vars and funcs
+  let text = "change the h1."
+  const textChange = function(){
+    const element = document.querySelector('h1');
+    element.textContent = text;
+  }
 
+  //Return public funcs and vars
+  return {
+    callTectChange: function(){
+      textChange();
+      console.log(text);
+    }
+  }
+})();
+UIcontroller.callTectChange();
